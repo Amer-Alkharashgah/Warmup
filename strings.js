f824d8676919e7cfaf20c7a6487bebf0a6c16121
Amer-Alkharashgah <rbk-12@RBK-12s-Mini.Dlink>
2019-03-20 09:35:44 +0200

    solution

--- strings.js
+++ strings.js
@@ -2,6 +2,15 @@
 // string string exactly n times.
 // repeatStr("hello",4) ==> 'hellohellohellohello'
 	
-	function repeatStr (n, s) {
-		//your code is here
+	function repeatStr (s, n) {
+
+     var output="";  //// new variable for the string output
+		while(n>0)
+  { 
+
+  	   output+=s;           ///  adding the new string every time
+  	   n--;
+  }
+   return output;            ///returning the output 
+		//your code is here  
 	}
